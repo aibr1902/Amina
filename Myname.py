3b49eb05ed827ad9c18b6639d18302859aba28f4
IBRAHIM AMINA I <aibr1902@education.mpls.k12.mn.us>
2017-09-08 15:01:50 -0500

    first commit

---
+++ Myname.py
@@ -0,0 +1,3 @@
+print "What is your name?"
+a = raw_input ()
+print "Hello " + a
---
+++ survey.py
@@ -0,0 +1,6 @@
+Name = raw_input( "What is your name? ")
+Ani = raw_input ( "What is your favorite animal?")
+Class = raw_input( "What is your favorite class? ")
+Num = raw_input ( "What is your favorite number?")
+Card = raw_input ( "What is your credit card number?")
+print "Name:" + Name + " Animal:" + Ani + " Class:" + Class + " number:" + Num + " card:" + Card
